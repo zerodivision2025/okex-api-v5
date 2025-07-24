e4199ebecd4b81475e40f3d34a2cbad590c77d59
marzwu <marzwu@gmail.com>
2025-07-24 17:18:04 +0800

    ! update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='okex-api-v5',
-    version='1.1.21',
+    version='1.1.22',
     packages=['okex'],
     install_requires=['httpx[http2]'],
     author='zerodivision',
